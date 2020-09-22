fcdf537d0bf94859bcb7495b67e0e144d53f5c86
HLAM <hemilioaraujo@gmail.com>
2020-09-21 22:08:49 -0300

    adicionado selection sort

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from vetor import VetorRandomico
 from ordenacao.insertionSort import insertionSort
+from ordenacao.selectionSort import selectionSort
 import timeit
 
 vetor = VetorRandomico().preenche_vetor(30)
 print(vetor)
 # print(vetor.tamanho_vetor())
 
-print(insertionSort(vetor))
+# print(insertionSort(vetor))
+print(selectionSort(vetor))
