b6cd435b93f929d0b614c67b1a5d4c406c7542d8
HLAM <hemilioaraujo@gmail.com>
2020-09-24 22:07:25 -0300

    adicionado contador do bubble sort

--- ordenacao/bubbleSort.py
+++ ordenacao/bubbleSort.py
@@ -2,15 +2,19 @@ from os import name
 
 
 def bubbleSort(arr): 
-	n = len(arr) 
+    trocas = 0
+    n = len(arr)
+	
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                trocas += 1
 
-	for i in range(n-1): 
-		for j in range(0, n-i-1): 
-			if arr[j] > arr[j+1] : 
-				arr[j], arr[j+1] = arr[j+1], arr[j] 
+    return arr, trocas
 
 if __name__ == "__main__":
-    arr = [64, 34, 25, 12, 22, 11, 90]
+    arr = [2, 4, 1, 6, 5, 3]
     print(arr)
-    bubbleSort(arr)
-    print(arr)
+    vetor, trocas = bubbleSort(arr)
+    print(vetor, trocas)
