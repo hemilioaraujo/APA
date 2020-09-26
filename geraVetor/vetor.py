34addf68cbdad8aa13881230e522ea3398d2deff
hemilioaraujo <hemilioaraujo@gmail.com>
2020-09-26 12:57:15 -0300

    adicionada funçao print_resultados

--- geraVetor/vetor.py
+++ geraVetor/vetor.py
@@ -8,7 +8,8 @@ class VetorRandomico:
     def preenche_vetor(self, n):
         for i in range(n):
             self.vetor.append(randint(10000, 99999))
-        return self.vetor
+            arr = self.vetor
+        return arr
 
     def print_vetor(self):
         for i in self.vetor:
@@ -16,3 +17,23 @@ class VetorRandomico:
 
     def tamanho_vetor(self):
         return len(self.vetor)
+
+    def print_resultados(vetor, ordenado, tipo, trocas):
+        print('\n**************************************************************************')
+        print(f'                              {tipo}')
+        print('**************************************************************************\n')
+
+        print('\n**************************************************************************')
+        print(f'                           VETOR DESORDENADO')
+        print('**************************************************************************\n')
+        print(vetor)
+
+        print('\n**************************************************************************')
+        print(f'                            VETOR ORDENADO')
+        print('**************************************************************************\n')
+        print(ordenado)
+
+        print('\n**************************************************************************')
+        print(f'                       TROCAS NECESSÁRIAS {trocas}')
+        print('**************************************************************************\n')
+
