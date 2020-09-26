04869b413e4c84d4b811362c595110ad76977774
hemilioaraujo <hemilioaraujo@gmail.com>
2020-09-26 11:49:56 -0300

    adicionado contador ao heapsort

--- ordenacao/heapSort.py
+++ ordenacao/heapSort.py
@@ -1,7 +1,10 @@
+trocas = 0
+
 def heapify(arr, n, i):
 	largest = i
 	l = 2 * i + 1
 	r = 2 * i + 2
+	global trocas
 
 	if l < n and arr[i] < arr[l]:
 		largest = l
@@ -11,24 +14,25 @@ def heapify(arr, n, i):
 
 	if largest != i:
 		arr[i],arr[largest] = arr[largest],arr[i]
-
+		trocas += 1
 		heapify(arr, n, largest)
 
 
 def heapSort(arr):
 	n = len(arr)
+	global trocas 
 
 	for i in range(n//2 - 1, -1, -1):
 		heapify(arr, n, i)
 
 	for i in range(n-1, 0, -1):
 		arr[i], arr[0] = arr[0], arr[i]
+		trocas += 1
 		heapify(arr, i, 0)
+	return arr, trocas
 
-if __name__ == "__main__":    
-    arr = [ 12, 11, 13, 5, 6, 7]
-    heapSort(arr)
-    n = len(arr)
-    print ("Sorted array is")
-    for i in range(n):
-        print ("%d" %arr[i])
+if __name__ == "__main__":
+	arr = [ 12, 11, 13, 5, 6, 7]
+	print(arr)
+	vetor, trocas = heapSort(arr)
+	print(vetor, trocas)
